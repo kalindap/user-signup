d7e69a8fedabd3f57f536fa59cecbcd2984e4288
Kalinda Piper <kalindap@gmail.com>
2017-04-03 13:55:31 -0500

    user taken to sign-in page by entering valid passwords; user given error messages if username, passwords or email address not valid; username and email are preserved if error shown

--- main.py
+++ main.py
@@ -15,42 +15,158 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+import re
 
 form = """
 <!DOCTYPE html>
 <html>
     <head>
-        <title>User Sign-Up</title>
+        <title>Sign-Up</title>
+        <style>
+            .error {
+                color: red;
+            }
+        </style>
     </head>
     <body>
         <h1>Sign-Up</h1>
-        <form>
-            <label>Username
-            <input name="username" type="text">
-            </label>
-            <br>
-            <label>Password
-            <input name="password" type="text">
-            </label>
-            <br>
-            <label>Verify Password
-            <input name="verify-password" type="text">
-            </label>
-            <br>
-            <label>Email (optional)
-            <input name="email" type="text">
-            </label>
-            <br>
+        <form method="post">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <label>Username</label>
+                        </td>
+                        <td>
+                            <input name="username" type="text" value="%(username)s">
+                            <span class="error">%(username_error)s</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Password</label>
+                        </td>
+                        <td>
+                            <input name="password" type="password">
+                            <span class="error">%(valid_password_error)s</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Verify Password</label>
+                        </td>
+                        <td>
+                            <input name="verify-password" type="password">
+                            <span class="error">%(password_match_error)s</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Email (optional)</label>
+                        </td>
+                        <td>
+                            <input name="email" type="text" value="%(email)s">
+                            <span class="error">%(email_error)s</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
             <input type="submit">
         </form>
     </body>
 </html>
 """
 
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    if not USER_RE.match(username):
+        return False
+    else:
+        return True
+
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    if not PASSWORD_RE.match(password):
+        return False
+    else:
+        return True
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+def valid_email(email):
+    if not EMAIL_RE.match(email):
+        return False
+    else:
+        return True
+
+
 class MainHandler(webapp2.RequestHandler):
+
+    def write_form(self, username_error="", valid_password_error="", password_match_error="", email_error="", username="", email=""):
+        #get user inputs for username and email for sake of preserving user's input if form is rerendered
+        user_username = self.request.get("username")
+        user_email = self.request.get("email")
+
+        username = cgi.escape(user_username, quote=True)
+        email = cgi.escape(user_email, quote=True)
+
+        self.response.write(form % {"username_error": username_error,
+                                    "valid_password_error": valid_password_error,
+                                    "password_match_error": password_match_error,
+                                    "email_error": email_error,
+                                    "username": username,
+                                    "email":email})
+
     def get(self):
-        self.response.write(form)
+        self.write_form()
+
+    def post(self):
+        #get user inputs
+        user_username = self.request.get("username")
+        user_password = self.request.get("password")
+        user_verify_input = self.request.get("verify-password")
+        user_email = self.request.get("email")
+
+        #html escape user inputs
+        username = cgi.escape(user_username, quote=True)
+        password = cgi.escape(user_password, quote=True)
+        verify_input = cgi.escape(user_verify_input, quote=True)
+        email = cgi.escape(user_email, quote=True)
+
+        #check if valid username
+        username_error = ""
+        if valid_username(username) == False:
+            username_error="Username not valid."
+
+        #check if valid password
+        valid_password_error = ""
+        if valid_password(password) == False:
+            valid_password_error="Password not valid."
+
+        #check if password and verify password are the same
+        password_match_error = ""
+        if password != verify_input:
+            password_match_error="Your passwords didn't match."
+
+        #check if valid email address
+        email_error = ""
+        if email and valid_email(email) == False:
+            email_error="Email address not valid."
+
+        #if everything valid, go to welcome page
+        if username_error or valid_password_error or password_match_error or email_error:
+            self.write_form(username_error, valid_password_error, password_match_error, email_error)
+        else:
+            self.redirect("/welcome/?username=%(username)s" % {"username":username})
+
+
+class WelcomeHandler(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get("username")
+        self.response.write("Welcome " + username + "!")
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome/', WelcomeHandler)
 ], debug=True)
